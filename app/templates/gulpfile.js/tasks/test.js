fe0b33226c567f6cff723e2b39ca9846dd1aea0f
Rolly Fordham <rolly@luma.co.nz>
2015-08-03 14:18:08 +1000

    tidy up test flags

--- app/templates/gulpfile.js/tasks/test.js
+++ app/templates/gulpfile.js/tasks/test.js
@@ -1,21 +1,53 @@
 'use strict';
 var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var jasmine = require('gulp-jasmine');
 var argv = require('yargs').argv;
-var Jasmine = require('jasmine');
 var reporters = require('jasmine-reporters');
+var jasmineConf = require('../../tests/support/jasmine.json');
 
 gulp.task('test', function(cb) {
-  var jasmine = new Jasmine();
-  jasmine.loadConfigFile('tests/support/jasmine.json');
+  var testOptions = {};
 
-  jasmine.getEnv().addReporter(new reporters.JUnitXmlReporter({
-      savePath: './reports',
-      consolidateAll: false
-  }));
-
-  if (argv.coverage) {
-    // @TODO
+  if (argv.verbose) {
+    testOptions.verbose = argv.verbose === true;
   }
 
-  jasmine.onComplete(cb);
+  if (argv.includeStackTrace) {
+    testOptions.includeStackTrace = argv.includeStackTrace === true;
+  }
+
+  if (argv.timeout) {
+    testOptions = argv.timeout;
+  }
+
+  testOptions.reporter = [
+    new reporters.JUnitXmlReporter({
+        savePath: './reports',
+        consolidateAll: false
+    }),
+
+    new reporters.TerminalReporter({
+        showStack: !!testOptions.includeStackTrace,
+        verbosity: 3,
+        color: true
+    })
+  ];
+
+  if (argv.coverage === true) {
+    gulp.src(['src/**/*.js', 'app.js'])
+      .pipe(istanbul()) // Covering files
+      .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+      .on('finish', function() {
+        gulp.src(jasmineConf.spec_files)
+          .pipe(jasmine(testOptions))
+          .pipe(istanbul.writeReports()) // Creating the reports after tests ran
+          .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } })) // Enforce a coverage of at least 90%
+          .on('end', cb);
+      });
+  } else {
+    gulp.src(jasmineConf.spec_files)
+      .pipe(jasmine(testOptions))
+      .on('end', cb);
+  }
 });
