bbf8ef5ca0083303a05c356b4bade2626d8b4dfa
Rolly Fordham <rolly@luma.co.nz>
2015-08-27 10:19:07 +1000

    run browser and node tests in parallel

--- app/templates/gulpfile.js/tasks/test.js
+++ app/templates/gulpfile.js/tasks/test.js
@@ -3,4 +3,4 @@
 var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence').use(gulp);
 
-gulp.task('test', gulpSequence('lint', 'test-node', 'test-browser'));
+gulp.task('test', gulpSequence('lint', ['test-node', 'test-browser']));
