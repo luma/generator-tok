7d7a0655d4427b8b56ad92636e8207c85ad6c86f
Rolly Fordham <rolly@luma.co.nz>
2015-11-09 10:42:35 +1100

    fix up the todo task and allow it to scan the tests dir

--- app/templates/gulpfile.js/tasks/todo.js
+++ app/templates/gulpfile.js/tasks/todo.js
@@ -3,21 +3,24 @@
 var gulp = require('gulp');
 var todo = require('gulp-todo');
 var config = require('../config.js');
+var jasmineConf = require('../../tests/support/jasmine.json');
+var srcPath = config.src + '**/*.js';
+var testsPath = jasmineConf.spec_files.concat(jasmineConf.helpers).map(function(path) {
+  return path.replace('tests/', './tests**/');
+});
+var todoSearchPaths = testsPath.concat(srcPath);
+var through2 = require('through2');
 
 // output once in markdown and then output a json file as well
 gulp.task('todo', function() {
-  console.log(config.src + '/**/*.js');
-  var s = gulp.src(config.src + '/**/*.js')
-              .pipe(todo.reporter('json', { fileName: 'todo.json' }))
-              .pipe(gulp.dest('./')) //output todo.json as json
-              .pipe(todo())
-              .pipe(gulp.dest('./')); //output todo.md as markdown
-
-  s.on('data', function(chunk) {
-    process.stdout.write(chunk.contents.toString());
-  });
-
-  s.on('end', function() {
-    process.stdout.write('\n\n');
-  });
+  gulp.src(todoSearchPaths)
+            .pipe(todo())
+            .pipe(gulp.dest('./')) //output todo.md as markdown
+            .pipe(todo.reporter('json', { fileName: 'TODO.json' }))
+            .pipe(gulp.dest('./')) //output todo.json as json
+            .pipe(todo.reporter('table', { fileName: 'TODO.json' }))
+            .pipe(through2.obj(function(obj, enc, next) {
+              this.push(obj.contents);
+              next();
+            })).pipe(process.stdout);
 });
